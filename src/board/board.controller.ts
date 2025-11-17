cda5219d366b2123b97c20a9000c8437f2021c2c
DoyoSaee <ehdgydi@gmail.com>
2025-11-17 23:34:40 +0900

    feat: add initial User CRUD controller

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post } from '@nestjs/common';
 
 @Controller('board')
-export class BoardController {}
+export class BoardController {
+  @Get()
+  findAll() {
+    return 'findAll';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `findOne ${id}`;
+  }
+
+  @Post()
+  create() {
+    return 'create';
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return `delete ${id}`;
+  }
+}
