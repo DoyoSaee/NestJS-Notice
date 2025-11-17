c2687a0ec2bdebfb442fe26d0a31f8437f9b046d
DoyoSaee <ehdgydi@gmail.com>
2025-11-17 23:44:51 +0900

    chore : add board module

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -1,24 +1,42 @@
-import { Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 
 @Controller('board')
 export class BoardController {
+  //전체 조회
   @Get()
   findAll() {
     return 'findAll';
   }
 
+  //단건 조회
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id') id: number) {
     return `findOne ${id}`;
   }
 
+  //생성
   @Post()
-  create() {
-    return 'create';
+  create(@Body() body: any) {
+    return 'create' + body;
   }
 
+  //수정
+  @Put(':id')
+  update(@Param('id') id: number, @Body() body: any) {
+    return `update ${id}` + body;
+  }
+
+  //삭제
   @Delete(':id')
-  delete(@Param('id') id: string) {
+  delete(@Param('id') id: number) {
     return `delete ${id}`;
   }
 }
