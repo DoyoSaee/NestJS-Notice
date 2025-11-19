c579c736fe12cb4ffea6a5fca3136ab86716e573
DoyoSaee <ehdgydi@gmail.com>
2025-11-19 23:33:29 +0900

    feat: Add validation to CreateBoardDto using class-validator

--- src/board/dto/board.dto.ts
+++ src/board/dto/board.dto.ts
@@ -1,9 +1,16 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, MaxLength, MinLength } from 'class-validator';
 
 export class CreateBoardDto {
+  @MinLength(2)
+  @MaxLength(20)
+  @IsNotEmpty()
   @ApiProperty({ description: '게시글 제목', example: '새로운 게시글 제목' })
   title: string;
 
+  @MinLength(2)
+  @MaxLength(20)
+  @IsNotEmpty()
   @ApiProperty({ description: '작성자 이름', example: '홍길동' })
   name: string;
 }
